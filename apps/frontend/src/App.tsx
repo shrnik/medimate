65a83c1da1f06cd19f7bebe14d231e66331bd6dc
Hanako21 <72812688+Hanako21@users.noreply.github.com>
2024-11-24 16:32:09 -0500

    Added frontend routing.
    1.Added in the front end a pages folder
    2. added basic pages for home.tsx, form.tsx, records.tsx, and layout_sidebar.tsx. layout_sidebar.tsx is a parent of home.tsx and records.tsx.
    3. In app.tsx I added a Browser Routes to mange all the routes and their properties.

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout_sidebar from "./pages/layout_sidebar";
+import Home from "./pages/home";
+import Records from "./pages/records";
+import Form from "./pages/form";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout_sidebar />}>
+            <Route path="home" element={<Home />} />
+            <Route path="records" element={<Records />} />
+          </Route>
+          <Route path="/form" element={<Form />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ apps/frontend/src/pages/layout_sidebar.tsx
@@ -0,0 +1,25 @@
+import { Outlet, Link } from "react-router-dom";
+
+const layout_sidebar = () => {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/records">Records</Link>
+          </li>
+          <li>
+            <Link to="/form">Form</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Outlet />
+    </>
+  );
+};
+
+export default layout_sidebar;
